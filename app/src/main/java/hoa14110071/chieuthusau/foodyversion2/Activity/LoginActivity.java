5c4ea61385d23f57e9d2521f6997edc901490fd7
minhh <minhhoa3030@gmail.com>
2017-05-19 14:34:12 +0700

    FoodyVersion2

--- app/src/main/java/hoa14110071/chieuthusau/foodyversion2/Activity/LoginActivity.java
+++ app/src/main/java/hoa14110071/chieuthusau/foodyversion2/Activity/LoginActivity.java
@@ -1,15 +1,31 @@
 package hoa14110071.chieuthusau.foodyversion2.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import hoa14110071.chieuthusau.foodyversion2.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText txt_email;
+    private EditText txt_pass;
+    private Button  btn_dangnhap;
+    private Button  btn_dangky;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_dangky = (Button) findViewById(R.id.btn_dangky);
+        btn_dangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/hoa14110071/chieuthusau/foodyversion2/Activity/ProfileActivity.java
+++ app/src/main/java/hoa14110071/chieuthusau/foodyversion2/Activity/ProfileActivity.java
@@ -1,15 +1,29 @@
 package hoa14110071.chieuthusau.foodyversion2.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import hoa14110071.chieuthusau.foodyversion2.Fragment.fragmentEatWhat;
 import hoa14110071.chieuthusau.foodyversion2.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    LinearLayout btnDangNhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btnDangNhap = (LinearLayout) findViewById(R.id.buttonDangNhap);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),LoginActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
     }
 }
